c9c3a5dec7582d593cfb5251130dd833ede779d7
Adina Margolis <adinazissel@gmail.com>
2018-02-27 02:01:52 -0500

    Presidential Eligibility - Class Librar,Console App, and Windows Forms App

---
+++ PresidentialEligibility/PresidentialEligibility-PartC/Program.cs
@@ -0,0 +1,106 @@
+﻿using PresidentialEligibility_partA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresidentialEligibility_PartB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualEligibility ie = new IndividualEligibility();
+            Boolean eligible = false;
+
+            //user passed in arguments to the command line
+            if (args.Length >=1)  
+            {
+                
+                if (args[0] == "true")
+                {
+                    ie.setPlaceBirth(true);
+                }
+                else
+                {
+                    ie.setPlaceBirth(false);
+                }
+
+                ie.setBirthYear(Int32.Parse(args[1]));
+                ie.setYearsResidedInUS(Int32.Parse(args[2]));
+                ie.setPriorTermsServed(Int32.Parse(args[3]));
+                if (args[4] == "false")
+                {
+                    ie.setRebelledAgainstUS(false);
+                }
+                else
+                {
+                    ie.setRebelledAgainstUS(true);
+                }
+
+                 eligible = ie.determineEligibility();
+                if(eligible)
+                {
+                    Console.WriteLine("You are eligible to run for president of the United States!");
+                }
+                else
+                {
+                    Console.WriteLine("You are NOT eligible to run for president of the United States.");
+                }
+                   
+    
+            }
+
+
+            //the user passed in no arguments. Prompt them instead:
+            else
+            {
+
+                Console.WriteLine("\nWere you born in the United States? Enter yes or no.");
+
+                    if (Console.ReadLine() == "yes")
+                {
+                    ie.setPlaceBirth(true);
+                }
+                    else
+                {
+                    ie.setPlaceBirth(false);
+                }
+
+                Console.WriteLine("\nWhat is your birth year? (Please enter all 4 digits)");
+                ie.setBirthYear(Int32.Parse(Console.ReadLine()));
+
+                Console.WriteLine("\nHow many years have you resided in the United States?");
+                ie.setYearsResidedInUS(Int32.Parse(Console.ReadLine()));
+
+                Console.WriteLine("\nHow many prior terms have you served?");
+                ie.setPriorTermsServed(Int32.Parse(Console.ReadLine()));
+
+                Console.WriteLine("\nHave you ever rebelled against the United States? Enter yes or no.");
+                if (Console.ReadLine()=="no")
+                {
+                    ie.setRebelledAgainstUS(false);
+                }
+                else
+                {
+                    ie.setRebelledAgainstUS(true);
+                }
+
+                eligible = ie.determineEligibility();
+                if (eligible)
+                {
+                    Console.WriteLine("You are eligible to run for president of the United States!");
+                }
+                else
+                {
+                    Console.WriteLine("You are NOT eligible to run for president of the United States.");
+                }
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PresidentialEligibility/PresidentialEligibility-partB/IndividualEligibility.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresidentialEligibility_partA
+{
+    public class IndividualEligibility
+    {
+       private Boolean naturalBorn;
+        private Int32 birthYear;
+       private int yearsResidedUS;
+       private  int priorTerms;
+       private  Boolean rebelledAgainstUS;
+
+        public IndividualEligibility()
+        {
+            naturalBorn = false;
+            rebelledAgainstUS = false;
+        }
+
+        public void setPlaceBirth(Boolean naturalBorn)
+        {
+            this.naturalBorn = naturalBorn;
+        }
+
+        public void setBirthYear(Int32 birthYear)
+        {
+            if (birthYear.ToString().Length == 4 && birthYear < DateTime.Now.Year 
+                && DateTime.Now.Year - birthYear <= 120 )
+            {
+                this.birthYear = birthYear;
+            }
+
+            else
+            {
+                throw new InvalidOperationException("Year was invalid.");
+            }
+
+
+       }
+
+        public void setYearsResidedInUS(int  yearsResidedUS)
+        {
+            if (yearsResidedUS >=0 )
+            {
+                this.yearsResidedUS = yearsResidedUS;
+
+            }
+
+            else
+            {
+                throw new InvalidOperationException("Years entered must be a positive number.");
+            }
+        }
+
+        public void setPriorTermsServed(int priorTerms)
+        {
+            if (priorTerms >=0)
+            {
+                this.priorTerms = priorTerms;
+            }
+            else
+            {
+                throw new InvalidOperationException("Number of prior terms must be greater than or equal to zero.");
+            }
+
+        }        
+           
+        public void setRebelledAgainstUS(Boolean rebelledAgainstUS)
+        {
+            this.rebelledAgainstUS = rebelledAgainstUS;
+        }
+
+        public Boolean determineEligibility()
+        {
+            if(naturalBorn == true)
+            {
+                if (DateTime.Now.Year - birthYear >= 35)
+                {
+                    if(yearsResidedUS >= 14)
+                    {
+                        if (priorTerms ==0 || priorTerms ==1)
+                        {
+                            if(rebelledAgainstUS==false)
+                            {
+                                return true;  // person has all the factors so is eligible to run for president. Otherwise, return false - can't run.
+                            }
+                            return false;
+                        }
+                        return false;
+                    }
+                    return false;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+            }
+}
